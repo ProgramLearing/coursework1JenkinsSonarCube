93a2b6abfc13514080556ef8dfcee41ddd385888
“ProgramLearing” <cmille212@caledonian.ac.uk>
2025-03-06 07:36:42 +0000

    Added in python Coverage for sonarqube

--- Dec2Hex.py
+++ Dec2Hex.py
@@ -1,5 +1,9 @@
 import sys
 
+#Install Coverage - it is used to track coverag in python
+pip install coverage
+
+
 def decimal_to_hex(decimal_value):
     hex_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
     hexadecimal = ""
